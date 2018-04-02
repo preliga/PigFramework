a1bac4e45c9c1a1cc6706487ae5856c3c76f20d3
Piotr Religa <piotr00it@gmail.com>
2018-04-02 12:25:15 +0200

    Zmiany w autoload

--- App.php
+++ App.php
@@ -14,22 +14,7 @@ class App
 {
     public function run()
     {
-        spl_autoload_register(function ($class_name) {
-            $str = "../$class_name.php";
-            require $str;
-        });
-
-//        set_exception_handler(function(\Exception $exception){
-////            die(var_dump($exception));
-//            echo "Uncaught exception: " , $exception->getMessage(), "\n";
-//        });
-
-//        set_error_handler(function( int $errno , string $errstr, string $errfile, int $errline, array $errcontext ){
-//            die(var_dump($errno, $errstr, $errfile, $errline, $errcontext));
-//        });
-
         $baseUrl = $_SERVER['HTTP_HOST'] . '/';
-//        $url = empty($_SERVER['REDIRECT_URL']) ? (empty($_SERVER['REQUEST_URI']) ? '/' : $_SERVER['REQUEST_URI']) : $_SERVER['REDIRECT_URL'];
 
         $length = strpos($_SERVER['REQUEST_URI'], '?');
         $url = $length > 0 ? substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], '?')) : $_SERVER['REQUEST_URI'];
